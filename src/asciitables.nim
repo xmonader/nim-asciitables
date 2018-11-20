0d4d82fdde7186cf28314fadc0d97fe8934a8510
Ahmed T. Youssef <xmonader@gmail.com>
2018-11-20 13:23:02 +0200

    wip for supporting paddings in tables

--- src/asciitables.nim
+++ src/asciitables.nim
@@ -1,9 +1,38 @@
 import strformat, strutils
 
 
+type Cell = object
+  leftpad*: int
+  rightpad: int
+  pad*: int
+  text*: string
+
+proc newCell(text: string, leftpad=1, rightpad=1, pad=0): ref Cell =
+  result = new Cell
+  result.pad = pad
+
+  if pad != 0:
+    result.leftpad = pad
+    result.rightpad = pad
+  else:
+    result.leftpad = leftpad
+    result.rightpad = rightpad
+  result.text = text
+
+
+proc newCellFromAnother(another: ref Cell): ref Cell =
+  result = newCell(text=another.text, leftpad=another.leftpad, rightpad=another.rightpad)
+
+
+proc len*(this:ref Cell): int =
+  result = this.leftpad + this.text.len + this.rightpad
+
+proc `$`*(this:ref Cell): string =
+  result = " ".repeat(this.leftpad) & this.text & " ".repeat(this.rightpad)
+
 type AsciiTable = object 
   rows: seq[seq[string]]
-  headers: seq[string]
+  headers: seq[ref Cell]
   rowSeparator*: string
   colSeparator*: string 
   cellEdge*: string
@@ -23,9 +52,14 @@ proc newAsciiTable*(): ref AsciiTable =
   result.widths = newSeq[int]()
   result.suggestedWidths = newSeq[int]()
   result.rows = newSeq[seq[string]]()
-  result.headers = newSeq[string]()
+  result.headers = newSeq[ref Cell]()
 
 proc setHeaders*(this: ref AsciiTable, headers:seq[string]) =
+  for s in headers:
+    var cell = newCell(s)
+    this.headers.add(cell)
+
+proc setHeaders*(this: ref AsciiTable, headers: seq[ref Cell]) = 
   this.headers = headers
 
 proc setRows*(this: ref AsciiTable, rows:seq[seq[string]]) =
@@ -46,30 +80,33 @@ proc reset*(this:ref AsciiTable) =
   this.widths = newSeq[int]()
   this.suggestedWidths = newSeq[int]()
   this.rows = newSeq[seq[string]]()
-  this.headers = newSeq[string]()
+  this.headers = newSeq[ref Cell]()
 
 proc calculateWidths(this: ref AsciiTable) =
   var colsWidths = newSeq[int]()
   if this.suggestedWidths.len == 0:
-    for i in countup(1, this.headers.len):
-      colsWidths.add(0) 
+    for h in this.headers:
+      colsWidths.add(h.len) 
   else:
     colsWidths = this.suggestedWidths
   
   for row in this.rows:
     for colpos, c in row:
-      if len(c) > colsWidths[colpos]:
-        colsWidths[colpos] = len(c)
-      
+      var acell = newCellFromAnother(this.headers[colpos])
+      acell.text = c
+
+      if len(acell) > colsWidths[colpos]:
+        colsWidths[colpos] = len(acell)
+
   let sizeForCol = (this.tablewidth/len(this.headers)).toInt()
   var lenHeaders = 0
-  for h in this.headers:
-    lenHeaders += h.len()
+  for w in colsWidths:
+    lenHeaders += w 
 
   if this.tablewidth > lenHeaders:
+    if this.suggestedWidths.len == 0:
       for colpos, c in colsWidths:
-        if this.suggestedWidths.len == 0:
-          colsWidths[colpos] += sizeForCol - c
+        colsWidths[colpos] += sizeForCol - c
 
   this.widths = colsWidths
   
@@ -83,22 +120,31 @@ proc render*(this: ref AsciiTable): string =
   this.calculateWidths()
   # top border
   result &= this.oneline()
+  
   # headers
   for colidx, h in this.headers:
-      result &= this.colSeparator & h & " ".repeat(this.widths[colidx]-len(h) )
+    result &= this.colSeparator & $h & " ".repeat(this.widths[colidx]-len(h) )
+  
   result &= this.colSeparator
   result &= "\n"
+  # finish headers 
 
   # line after headers
   result &= this.oneline()
 
+  # start rows
   for r in this.rows:
+    # start row
     for colidx, c in r:
-      result &= this.colSeparator & c & " ".repeat(this.widths[colidx]-len(c)) 
+      let cell = newCell(c, leftpad=this.headers[colidx].leftpad, rightpad=this.headers[colidx].rightpad)
+      result &= this.colSeparator & $cell & " ".repeat(this.widths[colidx]-len(cell)) 
     result &= this.colSeparator
     result &= "\n"
     if this.separateRows: 
         result &= this.oneLine()
+    # finish row
+  
+  # don't duplicate the finishing line if it's already printed in case of this.separateRows
   if not this.separateRows:
       result &= this.oneLine()
   return result
@@ -106,16 +152,40 @@ proc render*(this: ref AsciiTable): string =
 proc printTable*(this: ref AsciiTable) =
   echo(this.render())
 
+when not defined(js):
+  import os, osproc
+  proc termColumns(): int =
+    if os.existsEnv("COLUMNS") and getEnv("COLUMNS").isDigit():
+        return parseInt(getEnv("COLUMNS"))
+    else:
+      if findExe("tput") != "":
+        let (cols, rc) = execCmdEx("tput cols")
+        if rc == 0:
+          return cols.strip().parseInt()
+      if findExe("stty") != "":
+        let (output, rc) = execCmdEx("stty size")
+        if rc == 0:
+          let parts = output.splitWhitespace()
+          if len(parts) == 2:
+            return parts[1].strip().parseInt()
+    
+    return 0
+
+
+
 when isMainModule:
+
+
+
   var t = newAsciiTable()
-  t.tableWidth = 80
+  t.tableWidth = termColumns()
   t.setHeaders(@["ID", "Name", "Date"])
   t.addRow(@["1", "Aaaa", "2018-10-2"])
   t.addRow(@["2", "bbvbbba", "2018-10-2"])
   t.addRow(@["399", "CCC", "1018-5-2"])
   printTable(t)
   
-  t.tableWidth = 0
+  # t.tableWidth = 0
   printTable(t)
 
   t.tableWidth = 0
